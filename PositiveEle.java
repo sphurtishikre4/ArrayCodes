90c2aa072dc6d1bfb78e3838527018cdd239cca5
sphurtishikre4 <sphurtishikre4@gmail.com>
2026-08-05 14:26:08 +0530

    Add files via upload
    Array Question - 21)WAP to print only +ve elements in an given array
    int[] a = {-10, 20, -30, 40, 50, -60};
    
    
    
    
    Positive Elements:
    20
    40
    50
    Output:20 40 50

---
+++ PositiveEle.java
@@ -0,0 +1,12 @@
+public class PositiveEle {
+    public static void main(String[] args) {
+
+        int[] a = {-10, 20, -30, 40, 50, -60};
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > 0) {
+                System.out.print(a[i] + " ");
+            }
+        }
+    }
+}
