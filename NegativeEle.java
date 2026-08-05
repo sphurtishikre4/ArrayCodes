37f25a45c81412090763ace3925a0360b8acfb6d
sphurtishikre4 <sphurtishikre4@gmail.com>
2026-08-05 14:25:08 +0530

    Add files via upload
    array  question- WAP to print only -ve elements in an given array
    int[] a = {-10, 20, -30, 40, 50, -60};

---
+++ NegativeEle.java
@@ -0,0 +1,12 @@
+public class NegativeEle {
+    public static void main(String[] args) {
+
+        int[] a = {-10, 20, -30, 40, 50, -60};
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < 0) {
+                System.out.println(a[i] + " ");
+            }
+        }
+    }
+}
