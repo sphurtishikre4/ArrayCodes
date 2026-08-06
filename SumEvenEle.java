161ca5df5270e22c6dd08ab1be6f0e214d17bdb1
sphurtishikre4 <sphurtishikre4@gmail.com>
2026-08-06 23:42:52 +0530

    Add files via upload
    WAP to Print Sum Even Element.

---
+++ SumEvenEle.java
@@ -0,0 +1,16 @@
+public class SumEvenEle {
+    public static void main(String[] args) {
+
+        int[] a = {10, 20, 30, 40, 50};
+
+        int sum = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 == 0) {
+                sum += a[i];
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
