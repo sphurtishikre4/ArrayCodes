b755ecf2d1da17674628fbe5c404a1bd509f24cb
sphurtishikre4 <sphurtishikre4@gmail.com>
2026-08-06 23:50:18 +0530

    Add files via upload
    Write a Program print count ofeven elements

---
+++ CountEven.java
@@ -0,0 +1,18 @@
+ public class CountEven {
+    public static void main(String[] args) {
+
+        int[] a = {10, 15, 20, 25, 30, 35};
+
+        int count = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 == 0) {
+                count++;
+            }
+        }
+
+        System.out.println("Count = " + count);
+    }
+} 
+    
+
